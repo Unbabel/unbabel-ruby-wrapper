b7ecabc0db98675ddb8931f8f781565751a5b015
David Silva <davidslv@gmail.com>
2014-11-01 23:26:51 +0000

    Adds find method in Translation

--- lib/unbabel/translation.rb
+++ lib/unbabel/translation.rb
@@ -1,6 +1,12 @@
 module Unbabel
   module Translation
 
+    # finds a translation by it's unique id
+    def find(uid)
+      response = Unirest.get("#{Unbabel::Client::ENDPOINT}/translation/#{uid}")
+      response.body
+    end
+
     # status are
     # 'new'         - The translation has been created and is being pre-processed
     # 'ready'       - The translation is ready to be processed in the unbabel platform.
