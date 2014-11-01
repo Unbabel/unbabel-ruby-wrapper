d8545b428b7f18ad6e3cf6eb9974ec3c5c52211d
David Silva <davidslv@gmail.com>
2014-11-01 11:27:32 +0000

    Adds specs for Unbabel::Client

--- lib/unbabel/client.rb
+++ lib/unbabel/client.rb
@@ -9,6 +9,28 @@ module Unbabel
       @token    = options[:token]
 
       yield(self) if block_given?
+      validate_credentials!
+    end
+
+    def credentials
+      {
+        username: username,
+        token:    token
+      }
+    end
+
+    def credentials?
+      credentials.values.all?
+    end
+
+    private
+    # Ensures that all necessary credentials
+    # are set during configuration.
+    def validate_credentials!
+      credentials.each do |credential, value|
+        next if !value.nil? && value.is_a?(String)
+        fail(ArgumentError.new("Invalid #{credential}, it's value: #{value.inspect} must be a string."))
+      end
     end
   end
 end
