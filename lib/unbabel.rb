268fb79c71f7797bc4d859739c75d8fd6f781f1c
David Silva <davidslv@gmail.com>
2014-11-01 23:47:26 +0000

    Tweaking - converting this lib into a gem

--- lib/unbabel.rb
+++ lib/unbabel.rb
@@ -1,5 +1,12 @@
-require 'httparty'
+module Unbabel
+  require 'pry'
+  require 'unirest'
 
-Dir[File.dirname(__FILE__) + '/unbabel/*.rb'].each do |file|
-  require file
+  Dir[File.dirname(__FILE__) + '/unbabel/*.rb'].each do |file|
+    require file
+  end
+
+  def self.new(options = {})
+    Unbabel::Client.new(options)
+  end
 end
