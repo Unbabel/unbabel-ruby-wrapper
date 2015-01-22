30a67b72e80ce74ad4f4a47b9a7b81a438b08065
David Silva <davidslv@gmail.com>
2015-01-22 23:55:25 +0000

    Adds Unbabel::Topic spec

--- spec/lib/unbabel/topic_spec.rb
+++ spec/lib/unbabel/topic_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe Unbabel::Topic do
+  include Unbabel::Topic
+
+  it 'returns an array of hashes' do
+    expected = [{
+      'topic' => {'name' => 'politics'},
+      'topic' => {'name' => 'gossip'}
+    }].to_json
+
+    stub_request(:get, "http://sandbox.unbabel.com/tapi/v2/topic")
+      .to_return(:body => {'objects' => expected }  )
+
+      expect(topics).to eq expected
+  end
 end
